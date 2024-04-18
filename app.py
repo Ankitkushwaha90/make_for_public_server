3429ab90dc16e392a7d929383efea214c7ecae65
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2024-04-18 04:32:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello():
 if __name__ == '__main__':
     # Run the Flask app on the private IP address of the server machine
     # Ensure the server is reachable from other devices in the private network
-    app.run(host='192.168.77.28', port=8080)
+    app.run(host='taking_your_private_ip', port=8080)
